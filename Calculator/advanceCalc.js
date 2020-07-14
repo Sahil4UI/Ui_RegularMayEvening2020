7d6fd7f710ff7a9b755235496e8fb8f413d57af0
Sahil4UI <54834362+Sahil4UI@users.noreply.github.com>
2020-07-15 00:26:11 +0530

    Add files via upload

--- Calculator/advanceCalc.js
+++ Calculator/advanceCalc.js
@@ -8,10 +8,50 @@ window.addEventListener("load",function()
    {
        numBtn[i].addEventListener("click",appendNum);
    }
+   var oprBtn= document.querySelectorAll(".opr");
+   for (var i=0;i<oprBtn.length;i++)
+   {
+       oprBtn[i].addEventListener("click",appendOpr);
+   }
+   document.querySelector(".result").addEventListener("click",Calc);
+   document.querySelector(".clear").addEventListener("click",clear);
 });
+
+var r1='';
+var flag =false;
 
 function appendNum()
 {   var textbox= document.querySelector("#box");
     // console.log(textbox);
-    textbox.value += event.srcElement.innerText;
+    r1+= event.srcElement.innerText;
+    textbox.value = r1;
+    flag= false;
+}
+
+function appendOpr()
+{   
+    var opr =event.srcElement.innerText;
+    if (!flag == true)
+    {
+        r1+=opr;
+        flag = true;
+    }
+    else{
+        r1 = r1.slice(0,r1.length-1);
+        r1+=opr;
+    }
+    document.querySelector("#box").value=r1;
+    
+    
+}
+
+function Calc()
+{
+    document.querySelector("#box").value = eval(document.querySelector("#box").value);
+    
+}
+function clear()
+{
+    document.querySelector("#box").value = "";
+    r1="";
 }
