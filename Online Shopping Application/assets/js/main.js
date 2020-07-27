3486f3541fc6fb4f91a3bae821227458598e55c6
Sahil4UI <54834362+Sahil4UI@users.noreply.github.com>
2020-07-27 18:13:58 +0530

    Add files via upload

--- Online Shopping Application/assets/js/main.js
+++ Online Shopping Application/assets/js/main.js
@@ -3,39 +3,56 @@ window.addEventListener("load", initEvents);
 function initEvents() {
     showProducts();
 }
+var row;
 
 function showProducts() {
-    var ul = document.querySelector("#products");
-    ul.innerHTMl = "";
+    var section = document.querySelector("#products");
+    row = document.createElement("div");
+    row.className = "row";
+    // ul.innerHTMl = "";
     products.forEach(function(obj) {
-        var li = document.createElement("li");
-        li.className = "products";
+        var div = document.createElement("div");
+        div.className = "products col-lg-4 col-md-6 col-sm-12";
+        div.setAttribute('title', obj.p_id);
         var p_name = document.createElement("span");
         p_name.innerHTML = obj.p_name;
 
         var p_price = document.createElement("span");
         p_price.innerHTML = obj.p_price;
 
-
         var p_image = document.createElement("img");
         // p_image.src = obj.p_image;
         p_image.setAttribute('src', obj.p_image);
         var cart_button = document.createElement("button");
         cart_button.innerHTML = "ADD TO CART";
-        cart_button.className = "btn btn-primary w-100"
-        li.appendChild(p_image);
-        li.appendChild(p_name);
-        li.appendChild(p_price);
-        li.appendChild(cart_button);
+        cart_button.className = "btn btn-primary w-100";
+        cart_button.addEventListener("click", add);
+        div.appendChild(p_image);
+        div.appendChild(p_name);
+        div.appendChild(p_price);
+        div.appendChild(cart_button);
 
-
-        ul.appendChild(li);
-
-
+        row.appendChild(div);
 
 
     })
+    var container = document.createElement("div");
+    container.className = "container-fluid";
+    container.appendChild(row);
+    section.appendChild(container);
 
 
+}
+
+function add() {
+    var elem = event.srcElement.parentNode;
+    var product = elem.childNodes;
+    var elemId = elem.title;
+    obj.addItem(elemId, product[1].innerHTML, product[2].innerHTML, product[0].src);
+    console.log(obj.itemList);
+    displayCartItem();
+}
+
+function displayCartItem() {
 
 }
---
+++ Online Shopping Application/assets/js/model.js
@@ -0,0 +1,15 @@
+var obj = {
+    itemList: [],
+    addItem: function(id, name, price, image) {
+        var item = new Item(id, name, price, image);
+        this.itemList.push(item);
+    }
+
+}
+
+function Item(id, name, price, image) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.image = image;
+}
